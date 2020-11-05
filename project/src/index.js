5befea8402b14f6279ef02cd8c7f61b2991670bb
webmaster <raffa.demello@gmail.com>
2020-11-04 22:51:12 -0300

    wrong files

--- project/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import "./assets/css/bootstrap4_utilities.css";
-import "./assets/sass/custom.css";
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
